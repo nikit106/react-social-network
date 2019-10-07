cd6fbca4855ff6c1bf9dd15f39e33cce098e7124
nikit106 <nikit106@gmail.com>
2019-10-07 20:28:51 +0300

    first commit

---
+++ src/componets/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return <div className = 'content'>
+            <div>
+                <img src = "https://www.w3schools.com/howto/img_snow_wide.jpg"></img>
+            </div>
+            <div>ava + description</div>
+            <div>
+                My posts
+                <div>
+                    news posts
+                </div>
+            </div>
+                <div>1</div>
+                <div>2</div>
+        </div>
+}
+    
+export default Profile;
