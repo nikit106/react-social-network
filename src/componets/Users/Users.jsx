f09f408bbf53e3246caabddc2a11f016917ffeb5
nikit106 <nikit106@gmail.com>
2019-10-10 15:42:59 +0300

    axios

--- src/componets/Users/Users.jsx
+++ src/componets/Users/Users.jsx
@@ -1,42 +1,24 @@
 import React from 'react';
 import styles from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
     
   if (props.users.length === 0) {
-    props.setUsers([
-         {id: 1, 
-        photoUrl: 'https://img1.liveinternet.ru/images/attach/d/1/131/379/131379541_ugryumuyy_pes_anuko_foto_3.jpg',
-        followed: true, 
-        fullName: 'Nikita', 
-        status: 'i am a boss', 
-        location: {sity: 'Moscow', country : 'Russia'}},
-        {id: 2, 
-        photoUrl: 'https://img1.liveinternet.ru/images/attach/d/1/131/379/131379541_ugryumuyy_pes_anuko_foto_3.jpg', 
-        followed: false, 
-        fullName: 'Dima', 
-        status: 'lets go', 
-        location: {sity: 'Minsk', country : 'Belarus'}},
-        {id: 3, 
-        photoUrl: 'https://img1.liveinternet.ru/images/attach/d/1/131/379/131379541_ugryumuyy_pes_anuko_foto_3.jpg', 
-        followed: true, 
-        fullName: 'Natasha', 
-        status: 'ok', 
-        location: {sity: 'Kiev', country : 'Ukraine'}},
-        {id: 4, 
-        photoUrl: 'https://img1.liveinternet.ru/images/attach/d/1/131/379/131379541_ugryumuyy_pes_anuko_foto_3.jpg', 
-        followed: true, 
-        fullName: 'Igor', 
-        status: 'yo', 
-        location: {sity: 'Minsk', country : 'Belarus'}}
-    ])
+      
+      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+          
+        props.setUsers(response.data.items)  ;       
+      });
   }  
+    
     return <div>
             {
                 props.users.map( u => <div key = {u.id}>
                                 <span>  
                                     <div>
-                                        <img src = {u.photoUrl} className = {styles.userPhoto} />
+                                        <img src = {u.photos.small != null ? u.photos.small : userPhoto} className = {styles.userPhoto} />
                                     </div>
                                     <div>
                                         {u.followed 
@@ -47,12 +29,12 @@ let Users = (props) => {
                                 </span>
                                 <span>
                                     <span>
-                                        <div>{u.fullName}</div>
+                                        <div>{u.name}</div>
                                         <div>{u.status}</div>
                                     </span>
                                     <span>
-                                        <div>{u.location.country}</div>
-                                        <div>{u.location.city}</div>
+                                        <div>{'u.location.country'}</div>
+                                        <div>{'u.location.city'}</div>
                                     </span>
                                 </span>
                 </div>)
